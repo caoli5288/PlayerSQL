5703193b1624bb09aec4c26a94dccfaa6dc1c07a
caoli5288 <caoli5288@gmail.com>
2015-10-22 12:54:47 +0800

    Bump version to 1.18

--- src/main/java/com/mengcraft/playersql/Events.java
+++ src/main/java/com/mengcraft/playersql/Events.java
@@ -1,7 +1,6 @@
 package com.mengcraft.playersql;
 
 import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,13 +8,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.mengcraft.playersql.SyncManager.State;
 import com.mengcraft.playersql.api.PlayerPreSwitchServerEvent;
@@ -23,7 +17,7 @@ import com.mengcraft.playersql.api.PlayerPreSwitchServerEvent;
 public class Events implements Listener {
 
     private final SyncManager manager;
-    private final DataCompound compond = DataCompound.DEFAULT;
+    private final DataCompound compound = DataCompound.DEFAULT;
     private final Main main;
 
     public Events(Main main) {
@@ -35,8 +29,8 @@ public class Events implements Listener {
     public void handle(PlayerPreSwitchServerEvent event) {
         if (!event.isCancelled()) {
             Player player = event.getPlayer();
-            if (compond.state(player.getUniqueId()) == null) {
-                compond.state(player.getUniqueId(), State.SWIT_WAIT);
+            if (compound.state(player.getUniqueId()) == null) {
+                compound.state(player.getUniqueId(), State.SWIT_WAIT);
                 manager.saveAndSwitch(player, event.getTarget());
             }
         }
@@ -45,18 +39,18 @@ public class Events implements Listener {
     @EventHandler
     public void handle(PlayerLoginEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
-        State state = compond.state(uuid);
+        State state = compound.state(uuid);
         if (state != null && state != State.CONN_DONE) {
             event.setResult(Result.KICK_OTHER);
             event.setKickMessage(DataCompound.MESSAGE_KICK);
         } else if (event.getResult() == Result.ALLOWED) {
-            compond.state(uuid, State.CONN_DONE);
+            compound.state(uuid, State.CONN_DONE);
         }
     }
 
     @EventHandler
     public void handle(PlayerJoinEvent event) {
-        compond.state(event.getPlayer().getUniqueId(),
+        compound.state(event.getPlayer().getUniqueId(),
                 State.JOIN_WAIT);
         if (Configs.MSG_ENABLE) event.getPlayer().sendMessage(
                 Configs.MSG_LOADING);
@@ -69,7 +63,7 @@ public class Events implements Listener {
     public void handle(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         UUID uuid = player.getUniqueId();
-        if (compond.state(uuid) == null) {
+        if (compound.state(uuid) == null) {
             manager.save(player, true);
         }
     }
@@ -78,7 +72,7 @@ public class Events implements Listener {
     public void handle(EntityDamageEvent event) {
         if (!(event.getEntity() instanceof Player)) return;
         UUID uuid = event.getEntity().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             event.setCancelled(true);
         }
     }
@@ -86,7 +80,7 @@ public class Events implements Listener {
     @EventHandler
     public void handle(PlayerPickupItemEvent event) {
     	UUID uuid = event.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             event.setCancelled(true);
         }
     }
@@ -94,7 +88,7 @@ public class Events implements Listener {
     @EventHandler
     public void handle(PlayerDropItemEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             event.setCancelled(true);
         }
     }
@@ -102,38 +96,30 @@ public class Events implements Listener {
     @EventHandler
     public void handle(PlayerInteractEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             event.setCancelled(true);
         }
     }
     
     @EventHandler
     public void handle(InventoryOpenEvent event) {
-    	if (compond.state(event.getPlayer().getUniqueId()) != null) {
+    	if (compound.state(event.getPlayer().getUniqueId()) != null) {
     		event.setCancelled(true);
     	}
     }
 
     @EventHandler
-    public void handle(PlayerInteractAtEntityEvent event) {
+    public void handle(PlayerInteractEntityEvent e) {
         UUID uuid = e.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
-            e.setCancelled(true);
-        }
-    }
-    
-    @EventHandler
-    public void handle(PlayerInteractEntityEvent event) {
-        UUID uuid = e.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             e.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void handle(PlayerCommandPreprocessEvent event) {
+    public void handle(PlayerCommandPreprocessEvent e) {
         UUID uuid = e.getPlayer().getUniqueId();
-        if (compond.state(uuid) != null) {
+        if (compound.state(uuid) != null) {
             e.setCancelled(true);
         }
     }
